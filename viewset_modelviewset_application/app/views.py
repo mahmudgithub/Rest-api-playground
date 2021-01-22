36b6f43a92e7f28a90d572ec04d0b03893db0535
mahmudgithub <mahmudhossain838@gmail.com>
2021-01-22 07:26:56 -0800

    complate view

--- viewset_modelviewset_application/app/views.py
+++ viewset_modelviewset_application/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import OneSerializer,TwoSerializer
+from .models import one,two
 
-# Create your views here.
+
+class OneViewset(viewsets.ModelViewset):
+    queryset=one.objects.all()
+    serializer_class = serializers.OneSerializer
+
+class TwoViewset(viewsets.ModelViewset):
+    queryset=two.objects.all()
+    serializer_class=serializers.TwoSerializer
