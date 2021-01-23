aa0775a83811e0c35652667edc8b076f177bccbb
mahmudgithub <mahmudhossain838@gmail.com>
2021-01-23 00:59:35 -0800

    add vires data

--- generic_retrivapi_views/app/views.py
+++ generic_retrivapi_views/app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 from.models import one
+from rest_framework import generics
+
+from .serializers import OneSerializers
+
+
+class OneViews(generics.RetrieveAPIView):
+    queryset=one.objects.all()
+    serializer_class=OneSerializers
+    
 
 
