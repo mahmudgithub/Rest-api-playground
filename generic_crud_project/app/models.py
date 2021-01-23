39f37edf66d497e38a4248cca4a7600c8dda0f86
mahmudgithub <mahmudhossain838@gmail.com>
2021-01-23 01:55:15 -0800

    add model

--- generic_crud_project/app/models.py
+++ generic_crud_project/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class one(models.Model):
+    name=models.CharField(max_length=100)
+    city=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class two(models.Model):
+    board=models.CharField(max_length=10)
+    roll=models.IntegerField()
+    def __str__(self):
+        return self.board
+
