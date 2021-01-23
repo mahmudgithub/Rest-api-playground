f463b24323b0cda4b3556d391ab02d81bbc7deda
mahmudgithub <mahmudhossain838@gmail.com>
2021-01-23 02:53:46 -0800

    add views and urls

--- generic_crud_project/app/urls.py
+++ generic_crud_project/app/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
+from.views import OneViews
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
+    path('api/<int:pk>/',OneViews.as_view(),name='home')
 ]
--- generic_crud_project/app/views.py
+++ generic_crud_project/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import one,two
+from .serializers import OneSerializer,TwoSerializer
 
-# Create your views here.
+class OneViews(generics.ListCreateAPIView):
+    queryset=one.objects.all()
+    serializer_class=OneSerializer
+
+class OneViews(generics.RetrieveUpdateDestroyAPIView):
+    queryset=one.objects.all()
+    serializer_class=OneSerializer
