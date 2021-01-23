e5be22a9a3ba5d6637615bdc4e1c021334d7bfba
mahmudgithub <mahmudhossain838@gmail.com>
2021-01-22 21:17:34 -0800

    initialize updateview project

---
+++ generic_updateapi_views/app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import generics
+from.models import one
+from.serializers import OneSerializer
+
+class OneViews(generics.updateAPIView):
+    queryset=one.objects.all()
+    serializer_class=OneSerializer
