49577a5064cd9bcd2abecce2f3a6f920fff13c9c
mahmudgithub <mahmudhossain838@gmail.com>
2021-01-24 04:59:14 -0800

    add some documents

--- generic_crud_project/app/views.py
+++ generic_crud_project/app/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from rest_framework import generics
+# from rest_framework.permissions import IsAdminUser
 from .models import one,two
 from .serializers import OneSerializer,TwoSerializer
+
+
 
 class OneViews(generics.ListCreateAPIView):
     queryset=one.objects.all()
     serializer_class=OneSerializer
-
+    # permission_classes = (permissions.IsAuthenticated,)
+    # permission_classes = [IsAdminUser]
+    
 class TwoViews(generics.RetrieveUpdateDestroyAPIView):
     queryset=one.objects.all()
     serializer_class=OneSerializer
+def get_object(self):
+    obj = get_object_or_404(self.get_queryset())
+    self.check_object_permissions(self.request, obj)
+    return obj
 
 
 
